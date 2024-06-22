5567a29e0d087fa7a595a0a826f9bf129c765634
RafaelQuadros1 <rquadros048@gmail.com>
2024-06-22 20:50:14 -0300

    upoi

--- my-pho/src/components/Chat.jsx
+++ my-pho/src/components/Chat.jsx
@@ -5,17 +5,27 @@ import './Chat.css';
 function Chat() {
     const [isFloatingElementOpen, setIsFloatingElementOpen] = useState(false);
 
-  // Função para abrir/fechar o elemento flutuante
-  const toggleFloatingElement = () => {
-    setIsFloatingElementOpen(!isFloatingElementOpen);
-  };
-  return (
-    <>
-    <div className='chat'>
-      <button onClick={toggleFloatingElement}><IoChatbubblesOutline style={{color: 'black', width: '30px', height: '30px', boxShadow:'0 0 5px #000000a8', borderRadius: '15%' }}/></button>
-    </div>
-    </>
-  );
+    // Função para abrir/fechar o elemento flutuante
+    const toggleFloatingElement = () => {
+        setIsFloatingElementOpen(!isFloatingElementOpen);
+    };
+    return (
+        <>
+            <div className='chat'>
+                <button onClick={toggleFloatingElement}><IoChatbubblesOutline style={{ color: 'black', width: '30px', height: '30px', boxShadow: '0 0 5px #000000a8', borderRadius: '15%' }} /></button>
+            </div>
+            {isFloatingElementOpen && (
+                <FloatingElementComponent />
+            )}
+        </>
+    );
 }
+const FloatingElementComponent = () => {
+    return (
+        <div className="floating_element">
+        <h1>oi</h1>
+        </div>
+    );
+};
 
 export default Chat;
