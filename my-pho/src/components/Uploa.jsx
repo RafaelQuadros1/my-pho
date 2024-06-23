5fd82b5f220e27380371dd7df1100ff1404e8f6f
RafaelQuadros1 <rquadros048@gmail.com>
2024-06-23 18:17:03 -0300

    upgrade

--- my-pho/src/components/Uploa.jsx
+++ my-pho/src/components/Uploa.jsx
@@ -4,11 +4,13 @@ import './Uploa.css';
 function Uploa() {
 
     return (
+        <>
         <div className="bouncing-balls">
-            <div class="ball"></div>
-            <div class="ball"></div>
-            <div class="ball"></div>
+            <span className="ball"></span>
+            <span className="ball"></span>
+            <span className="ball"></span>
         </div>
+        </>
     );
 }
 
