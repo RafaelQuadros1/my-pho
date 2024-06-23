50c010ee9af281268439a181a91d82928efd0f62
RafaelQuadros1 <rquadros048@gmail.com>
2024-06-22 22:05:17 -0300

    uopj

--- my-pho/src/components/Chat.jsx
+++ my-pho/src/components/Chat.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { IoChatbubblesOutline } from "react-icons/io5";
+import { IoIosArrowForward } from "react-icons/io";
 import './Chat.css';
 
 function Chat() {
@@ -12,7 +13,7 @@ function Chat() {
     return (
         <>
             <div className='chat'>
-                <button onClick={toggleFloatingElement}><IoChatbubblesOutline style={{ color: 'black', width: '30px', height: '30px', boxShadow: '0 0 5px #000000a8', borderRadius: '15%' }} /></button>
+                <button onClick={toggleFloatingElement}><IoChatbubblesOutline style={{ color: 'black', width: '30px', height: '30px' }} /></button>
             </div>
             {isFloatingElementOpen && (
                 <FloatingElementComponent />
@@ -23,7 +24,24 @@ function Chat() {
 const FloatingElementComponent = () => {
     return (
         <div className="floating_element">
-        <h1>oi</h1>
+            <div className='body_chat'>
+                <div className='men'>
+                <div className='igns'>
+                        <img src="" alt="" />
+                        <div className='text'>asd</div>
+                    </div> 
+                    <div className='user'>
+                        <img src="" alt="" />
+                        <div className='text'>asd</div>
+                    </div> 
+                </div>
+                <section className='inputs'>
+                    <form className='input_from'>
+                        <input type="text" placeholder="Digitir sua mensagem..." />
+                        <button><IoIosArrowForward /></button>
+                    </form>
+                </section>
+            </div>
         </div>
     );
 };
